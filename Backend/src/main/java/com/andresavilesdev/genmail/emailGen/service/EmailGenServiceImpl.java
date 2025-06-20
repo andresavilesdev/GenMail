53b8da6428a6b5f41e655bdee6da75b0ffd2b996
Andresavilesdev <andresaviles0721@gmail.com>
2025-06-19 22:27:35 -0500

    finishing de process to get de reslult

--- Backend/src/main/java/com/andresavilesdev/genmail/emailGen/service/EmailGenServiceImpl.java
+++ Backend/src/main/java/com/andresavilesdev/genmail/emailGen/service/EmailGenServiceImpl.java
@@ -1,8 +1,12 @@
 package com.andresavilesdev.genmail.emailGen.service;
 
 import com.andresavilesdev.genmail.emailGen.dto.EmailGenRequest;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.net.HttpHeaders;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Map;
 
@@ -13,6 +17,12 @@ public class EmailGenServiceImpl implements IEmailGenService{
     private String geminiApiUrl;
     @Value("${gemini.api.key}")
     private String geminiApiKey;
+
+    private final WebClient webClient;
+
+    public EmailGenServiceImpl(WebClient webClient) {
+        this.webClient = webClient;
+    }
 
     @Override
     public String generateEmailResponse(EmailGenRequest emailGenRequest) {
@@ -31,9 +41,33 @@ public class EmailGenServiceImpl implements IEmailGenService{
         );
 
         // DO REQUEST AND GET RESPONSE
-        // RETURN RESPONSE
 
-        return "";
+        String response = webClient.post()
+                .uri(geminiApiUrl + geminiApiKey)
+                .header(HttpHeaders.CONTENT_TYPE, "application/json")
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+
+        // EXTRACT RESPONSE ADN RETURN
+        return extractResponseContent(response);
+    }
+
+    private String extractResponseContent(String response) {
+
+        try{
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode = objectMapper.readTree(response);
+            return jsonNode.path("candidates").get(0)
+                    .path("content")
+                    .path("parts").get(0)
+                    .path("text")
+                    .asText();
+
+        } catch (Exception e) {
+            return "Error processing request: " + e.getMessage();
+        }
     }
 
     @Override
