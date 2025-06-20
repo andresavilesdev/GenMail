6f61e3e3ff1b079edfe2f8a2c6ce03a7ac52a21c
Andresavilesdev <andresaviles0721@gmail.com>
2025-06-20 14:21:47 -0500

    dockerizing

--- Backend/src/main/java/com/andresavilesdev/genmail/emailGen/controller/EmailGenController.java
+++ Backend/src/main/java/com/andresavilesdev/genmail/emailGen/controller/EmailGenController.java
@@ -3,13 +3,11 @@ package com.andresavilesdev.genmail.emailGen.controller;
 import com.andresavilesdev.genmail.emailGen.dto.EmailGenRequest;
 import com.andresavilesdev.genmail.emailGen.service.EmailGenServiceImpl;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/email")
+@CrossOrigin(origins = "*")
 public class EmailGenController {
 
     private final EmailGenServiceImpl emailGenService;
