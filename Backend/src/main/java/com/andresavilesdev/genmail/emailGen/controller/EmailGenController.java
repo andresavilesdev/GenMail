4cb5dc3dbbfc1f505d07f0bedc6dc043c0139b9f
Andresavilesdev <andresaviles0721@gmail.com>
2025-06-19 22:50:35 -0500

    creating the controller, adjusting routes and displaying result

--- Backend/src/main/java/com/andresavilesdev/genmail/emailGen/controller/EmailGenController.java
+++ Backend/src/main/java/com/andresavilesdev/genmail/emailGen/controller/EmailGenController.java
@@ -1,18 +1,29 @@
 package com.andresavilesdev.genmail.emailGen.controller;
 
 import com.andresavilesdev.genmail.emailGen.dto.EmailGenRequest;
+import com.andresavilesdev.genmail.emailGen.service.EmailGenServiceImpl;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/emailGen")
+@RequestMapping("/api/email")
 public class EmailGenController {
 
+    private final EmailGenServiceImpl emailGenService;
+
+    public EmailGenController(EmailGenServiceImpl emailGenService) {
+        this.emailGenService = emailGenService;
+    }
+
+    @PostMapping("/generate")
     public ResponseEntity<String> generateEmail(@RequestBody EmailGenRequest emailGenRequest) {
 
-        return ResponseEntity.ok("");
+        String response = emailGenService.generateEmailResponse(emailGenRequest);
+
+        return ResponseEntity.ok(response);
 
     }
 
