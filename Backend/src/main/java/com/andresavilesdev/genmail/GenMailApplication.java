97420fa1d5850260f5f81f21dc358be9d001560a
Andresavilesdev <andresaviles0721@gmail.com>
2025-06-20 10:38:06 -0500

    fixing test to deploy moment

--- Backend/src/main/java/com/andresavilesdev/genmail/GenMailApplication.java
+++ Backend/src/main/java/com/andresavilesdev/genmail/GenMailApplication.java
@@ -1,5 +1,6 @@
 package com.andresavilesdev.genmail;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GenMailApplication {
 
     public static void main(String[] args) {
+
+        // configuring environment variables
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+        );
+
         SpringApplication.run(GenMailApplication.class, args);
+
     }
 
 }
--- Backend/src/test/java/com/andresavilesdev/genmail/GenMailApplicationTests.java
+++ Backend/src/test/java/com/andresavilesdev/genmail/GenMailApplicationTests.java
@@ -1,10 +1,18 @@
 package com.andresavilesdev.genmail;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GenMailApplicationTests {
+public class GenMailApplicationTests {
+
+    @BeforeAll
+    static void setup() {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+    }
 
     @Test
     void contextLoads() {
